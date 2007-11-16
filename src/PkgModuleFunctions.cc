e31892897693d13e0966f4afae4249a2d5df4939
Ladislav Slezák <lslezak@suse.cz>
2007-11-16 11:49:19 +0000

    - remove temporary files /var/tmp/zypp-xmlstore-*-script-* at   the end: correctly call the destructor at the end, call   finishTarget() and release the zypp pointer (#228176) - 2.16.3
    svn path=/trunk/pkg-bindings/; revision=42117

--- src/PkgModuleFunctions.cc
+++ src/PkgModuleFunctions.cc
@@ -288,9 +288,22 @@ PkgModuleFunctions::~PkgModuleFunctions ()
 {
     delete &_callbackHandler;
 
-    y2milestone("Releasing the zypp pointer...");
-    zypp_pointer = NULL;
-    y2milestone("Zypp pointer released");
+    if (zypp_pointer != NULL)
+    {
+	try
+	{
+	    y2milestone("Finishing the target");
+	    zypp_pointer->finishTarget();
+	}
+	catch(...)
+	{
+	    y2error("finishTarget() has failed");
+	}
+
+	y2milestone("Releasing the zypp pointer...");
+	zypp_pointer = NULL;
+	y2milestone("Zypp pointer released");
+    }
 }
 
 Y2Function* PkgModuleFunctions::createFunctionCall (const string name, constFunctionTypePtr type)
