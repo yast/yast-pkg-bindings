484da0fc4a7a80173b19550d188f6192a579b33c
Stefan Schubert <schubi@suse.de>
2014-09-22 16:14:13 +0200

    taking item pointer

--- src/Resolvable_Properties.cc
+++ src/Resolvable_Properties.cc
@@ -217,7 +217,7 @@ YCPMap PkgFunctions::Resolvable2YCPMap(const zypp::PoolItem &item, const std::st
 	stat = "available";
     }
 
-    info->add(YCPString("onsystem_by_user"), YCPBoolean(item.satSolvable().isOnSystemByUser()));
+    info->add(YCPString("onsystem_by_user"), YCPBoolean(item->isOnSystemByUser()));
 
     info->add(YCPString("transact_by"), YCPSymbol(TransactToString(status.getTransactByValue())));
 
